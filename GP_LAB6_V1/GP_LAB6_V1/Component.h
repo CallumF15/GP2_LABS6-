d7f55b3428a3ea0209bb740509fce551611d2025
CallumF15 <Callum_flannagan@msn.com>
2014-11-03 22:28:27 +0000

    V1.1
    Created Component CPP & header. Fixed the header guard issues

---
+++ GP_LAB6_V1/GP_LAB6_V1/Component.h
@@ -0,0 +1,25 @@
+//Header guard - ensures class is compiled once & only once.
+#ifndef Component_h
+#define Component_h
+
+
+#include <string>
+
+
+
+
+class Component{
+
+public:
+	Component();
+	virtual ~Component(); //Destructor - Destroys the objects	virtual void init();	virtual void update();	virtual void render();	virtual void destroy();	const std::string &getType();	void toggleActive();	bool isActive();
+
+protected:
+	std::string m_Type;
+	bool m_Active;
+
+private:
+
+
+
+};#endif
